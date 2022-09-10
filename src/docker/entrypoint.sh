c2bbc810fee69098b27571a5b65a7e249bb63c7e
tom <tom@triton-it.nl>
2022-09-10 16:23:56 +0200

    Updated Setup

--- src/docker/entrypoint.sh
+++ src/docker/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/env bash
 
+while ! python -c "import MySQLdb; db=MySQLdb.connect(host=\"$DB_HOST\", passwd=\"$DB_PASS\")"; do
+    sleep 1
+done
+
 python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
--- src/settings/develop.py
+++ src/settings/develop.py
@@ -1,13 +1,15 @@
 import os
 
+from decouple import config
+
 from settings import *
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django-starter',
-        'HOST': 'db',
-        'USER': 'root',
-        'PASSWORD': 'secret'
+        'NAME': config("DB_NAME"),
+        'HOST': config("DB_HOST", "db"),
+        'USER': config("DB_USER"),
+        'PASSWORD': config("DB_PASS")
     }
 }
